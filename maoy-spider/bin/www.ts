9c0a586f94fc9f6e260da2ea363532e4ebe24f8f
xieshicoder <13233076263@163.com>
2018-01-12 00:03:27 +0800

    爬虫公共类

--- maoy-spider/bin/www.ts
+++ maoy-spider/bin/www.ts
@@ -1,6 +1,6 @@
 import { Server } from "../src/server";
 import * as log4js from "log4js";
-
+import {Spider} from '../src/utils/spider';
 
 let app = Server.bootstrap().app;
 let logger = log4js.getLogger('bootstrap');
@@ -8,4 +8,12 @@ let logger = log4js.getLogger('bootstrap');
 const server = app.listen(3000, "localhost", () => {
     let addr = server.address();
     logger.info("Express server listening on port ", addr.port, " with pid ", process.pid );
+});
+
+var spider = new Spider()
+// spider.getPage('http://m.maoyan.com/city?_v_=yes').then(data => {
+//     logger.info(data);
+// });
+spider.getJson('http://m.maoyan.com/movielist?_v_=yes',{offset: 0, limit: 100}).then(data => {
+    logger.info(data);
 });
---
+++ maoy-spider/src/utils/spider.ts
@@ -0,0 +1,51 @@
+import * as superagent from 'superagent';
+import * as cheerio from 'cheerio';
+import * as http from 'http';
+/**
+ * 爬虫公共类
+ */
+export class Spider {
+    constructor() {
+
+    }
+    /**
+     * @description 通过链接获取网页信息
+     * @param url 网页链接
+     */
+    public getPage(url: string): Promise<string>{
+        var promise = new Promise<string>((resolve, reject) => {
+            superagent.get(url)
+            .set('User-Agent','Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Mobile Safari/537.36')
+            .end((err, res) => {
+                if(!err){
+                    resolve(res.text);
+                }else {
+                    reject(err);
+                }
+            })
+        });
+        return promise;
+    }
+    /**
+     * @description 通过post请求获取json数据
+     * @param url--地址 
+     * @param data--表单数据
+     */
+    public getJson(url: string, data: object) {
+        var promise = new Promise<string>((resolve, reject) => {
+            superagent.post(url)
+            .set('User-Agent','Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Mobile Safari/537.36')
+            .set('Content-Type', 'application/x-www-form-urlencoded')
+            .send(data)
+            .end((err, res) => {
+                if(!err){
+                    resolve(res.text);
+                }else {
+                    reject(err);
+                }
+            })
+
+        })
+        return promise;
+    }
+}
