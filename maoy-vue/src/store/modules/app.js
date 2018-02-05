53487d829d909fb91319e29466285f4412180f20
xieshicoder <13233076263@163.com>
2018-02-06 00:35:42 +0800

    引入vuex控制状态

--- maoy-vue/src/store/modules/app.js
+++ maoy-vue/src/store/modules/app.js
@@ -1,18 +1,29 @@
 export default {
   state: {
-    currentIndex: 0
+    currentIndex: 0,
+    currentCity: {
+      cname: '北京',
+      id: 2189
+    }
   },
   getters: {
-    currentIndex: state => state.currentIndex
+    currentIndex: state => state.currentIndex,
+    city: state => state.city
   },
   mutations: {
     INDEX: (state, index) => {
       state.currentIndex = index
+    },
+    CITY: (state, city) => {
+      state.currentCity = city
     }
   },
   actions: {
     SET_INDEX ({commit}, data) {
       commit('INDEX', data)
+    },
+    SET_CITY ({commit}, data) {
+      commit('CITY', data)
     }
   }
 }
